1d569209ed1c4da8113f1a772f3ad72ef5e39173
darshan <darshan002321@gmail.com>
2023-11-03 22:57:36 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./styles.css";
 import { NewTodoForm } from "./NewTodoForm";
+import TodoList from "./TodoList";
 
 export default function App() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS")
+    if (localValue == null) return []
+
+    return JSON.parse(localValue)
+  });
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos))
+
+  }, [todos])
 
   function addTodo(title) {
      setTodos((currentTodos) => {
@@ -18,8 +29,8 @@ export default function App() {
   
 
   function toggleTodo(id, completed) {
-    setTodos((currentTodos) => {
-      return currentTodos.map((todo) => {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
         if (todo.id === id) {
           return { ...todo, completed };
         }
@@ -34,30 +45,12 @@ export default function App() {
     })
   }
 
-
+  
   return (
     <>
     <NewTodoForm onSubmit={addTodo}/>
       <h1 className="header">Todo List</h1>
-      <ul className="list">
-        {todos.length === 0 && "Type in the form box to add your To-Dos list"}
-        {todos.map(todo => {
-          return (
-            <li key={todo.id}>
-              <label>
-                <input
-                  type="checkbox"
-                  checked={todo.completed}
-                  onChange={(e) => toggleTodo(todo.id, e.target.checked)}
-                />
-                {todo.title}
-              </label>
-              <button onClick={() => deleteTodo(todo.id)} 
-              className="btn btn-danger">Delete</button>
-            </li>
-          );
-        })}
-      </ul>
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
     </>
   );
 }
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,23 +1,16 @@
-export default TodoList() {
+
+import { TodoItem } from "./TodoItem";
+
+
+export default function TodoList({ todos, toggleTodo, deleteTodo }) {
   return (
 
    <ul className="list">
-        {todos.length === 0 && "Type in the form box to add your To-Dos list"}
+        {todos.length === 0 && "no Todos"}
         {todos.map(todo => {
           return (
-            <li key={todo.id}>
-              <label>
-                <input
-                  type="checkbox"
-                  checked={todo.completed}
-                  onChange={(e) => toggleTodo(todo.id, e.target.checked)}
-                />
-                {todo.title}
-              </label>
-              <button onClick={() => deleteTodo(todo.id)} 
-              className="btn btn-danger">Delete</button>
-            </li>
-          );
+            <TodoItem {...todo} key={todo.id} toggleTodo={toggleTodo}  deleteTodo={deleteTodo} />
+          )
         })}
       </ul>
   )
